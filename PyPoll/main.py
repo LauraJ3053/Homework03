a40fc47b18af92c105c053ebfa2420de05e37587
Laura J <lelizabehtj@yahoo.com>
2019-03-31 20:36:13 -0400

    update to homework 3/31/2019

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,45 +1,51 @@
+# naming dependencies
 import os
 import csv
 
-file_to_load = os.path.join("Resources","election_data.csv")
+# files to load
+file_to_load = os.path.join("..","Resources","election_data.csv")
 file_to_output = "Resources/election_analysis.txt"
 print(file_to_load)
 
-cwd = os.getcwd()
-print(cwd)
+# checking pathway
+#cwd = os.getcwd()
+#print(cwd)
 
-#Varibles to Track
+# varibles
 total_votes = 0
 candidates = []
 perc_cand_vote = float
 sum_cand_vote = 0
 cand_options = {}
 
-with open(file_to_load) as election_data:
+# reading files
+with open(file_to_load) as election_data :
     reader = csv.DictReader(election_data)
 
-
-
+# looping through data
     for row in reader:
-        print(f"{row}")
+        #print(f"{row}")
+
+# calculating the totals
         total_votes = total_votes+ 1
-        candidates = row["Candidates"]
+        candidates = row["Candidate"]
 
-        if row["Candidates"] not in candidate:
+# adding candiates to the list Candidates
+        if row["Candidate"] not in candidates:
 
-            candidate.append(row["Candidates"])
+            candidates.append(row["Candidate"])
 
-            candidate_options[row["Candidates"]] = 1
+            candidate_options[row["Candidate"]] = 1
 
         else:
-            candidate_options[row["Candidates"]] = candidate_options[row["Candidates"]] + 1
+            candidate_options[row["Candidate"]] = candidate_options[row["Candidate"]] + 1
 
-#----------------------------------------------------------------------------------------
+
     # Determine the Winner:
-    #if (votes > winner_votes[2]):
-     #   greatest_increase[1] = revenue_change
-      #  greatest_increase[0] = row["Candidate"]
-#----------------------------------------------------------------------------------------
+        if (sum_cand_vote  > sum_cand_vote [2]):
+            highest_vote[1] = Row(int(sum_cand_vote))
+            highest_vote[0] = row["Candidate"]
+
 
     print()
     print()
@@ -53,9 +59,9 @@ with open(file_to_load) as election_data:
         print(candidate + " " + str(round(((candidate_options[candidates]/total_votes)*100))) + "%" + " (" + str(candidate_options[candidates]) + ")")
         candidate_results = (candidates + " " + str(round(((candidate_options[candidates]/total_votes)*100))) + "%" + " (" + str(candidate_options[candidates]) + ")")
 
-candidate_votes
+sum_cand_vote
 
-winner = sorted(candidate_votes.items(), key=itemgetter(1), reverse=True)
+winner = sorted(candidate_votes.items(), key=get_item(1), reverse=True)
 
 #results
 print("-------------------------")
@@ -63,21 +69,18 @@ print("Winner: " + str(winner[0]))
 print("-------------------------")
 
 
-
-
-
-# Output Files
+# Print
 with open(file_to_output, "w") as txt_file:
 
     txt_file.write("Election Results")
     txt_file.write("\n")
     txt_file.write("-------------------------")
     txt_file.write("\n")
-    #txt_file.write(candidate + " " + str(round(((candidate_votes[candidate]/votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")")
+    txt_file.write(candidate + " " + str(round(((sum_cand_vote[candidates]/total_votes)*100))) + "%" + " (" + str(sum_cand_vote[candidates]) + ")")
     txt_file.write(str(winner))
     txt_file.write("\n")
     txt_file.write("-------------------------")
     txt_file.write("\n")
     txt_file.write("Winner: " + str(winner[0]))
     txt_file.write("\n")
-    txt_file.write("Total Votes " + str(votes))
+    txt_file.write("Total Votes " + str(total_votes))
